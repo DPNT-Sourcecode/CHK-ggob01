114cf4603f35cc95f67c4025822971ee1c85a2fa
sbx_user1051 <sbx_user1051@169.254.5.209>
2023-08-23 13:27:56 +0000

    Wed Aug 23 13:27:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,7 +4,9 @@ import befaster.runner.SolutionNotImplementedException;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.*;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.HashMap;
 
 public class CheckoutSolution {
@@ -15,8 +17,13 @@ public class CheckoutSolution {
     }
 
     private HashMap<String, SkuPricing> buildPriceList() {
-        Object parsed = new JSONParser().parse(new FileReader("./skusRaw.json"));
+        try {
+            Object parsed = new JSONParser().parse(new FileReader("./skusRaw.json"));
+        } catch (IOException | ParseException e) {
+            lo
+        }
     }
 }
+
 
 
