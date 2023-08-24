4728198313d9f281c397dd7b64585a9181aef698
sbx_user1051 <sbx_user1051@169.254.11.237>
2023-08-24 14:57:56 +0000

    Thu Aug 24 14:57:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/Helper.java
+++ src/main/java/befaster/solutions/CHK/Helper.java
@@ -10,8 +10,8 @@ public class Helper {
     public static HashMap<String, SkuPricing> buildPriceList() {
         HashMap<String, SkuPricing> pricing = new HashMap<>();
 
-        pricing.put("A", new SkuPricing("A", 50, 3, 130));
-        pricing.put("B", new SkuPricing("B", 30, 2, 45));
+        pricing.put("A", new SkuPricing("A", 50, "sameProductDiscount", 3, 130));
+        pricing.put("B", new SkuPricing("B", 30, "sameProductDiscount", 2, 45));
         pricing.put("C", new SkuPricing("C", 20));
         pricing.put("D", new SkuPricing("D", 15));
 
@@ -19,3 +19,4 @@ public class Helper {
     }
 
 }
+
