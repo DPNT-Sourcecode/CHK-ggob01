75ad5d381857c55529c5a5fd9ea1ed77a22fb572
sbx_user1051 <sbx_user1051@169.254.11.237>
2023-08-24 19:05:34 +0000

    Thu Aug 24 19:05:34 UTC 2023

--- src/main/java/befaster/solutions/CHK/SameProductDiscount.java
+++ src/main/java/befaster/solutions/CHK/SameProductDiscount.java
@@ -9,37 +9,24 @@ public class SameProductDiscount extends Discounter{
         super();
     }
 
-//    public int priceForSku2(String sku, HashMap<String, Integer> aggregated) {
-//        int totalPrice = 0;
-//
-//        int singleChargeItems = aggregated.get(sku);
-//        int offerMultiple = priceList.get(sku).getOfferMultiple();
-//        if (aggregated.get(sku) >= offerMultiple && offerMultiple > 0) {
-//            totalPrice += (aggregated.get(sku) / offerMultiple) * priceList.get(sku).getOfferPrice();
-//            singleChargeItems = aggregated.get(sku) % offerMultiple;
-//        }
-//        totalPrice += singleChargeItems * priceList.get(sku).getPrice();
-//        return totalPrice;
-//    }
-
     @Override
     public int priceForSku(String sku, HashMap<String, Integer> aggregated) {
         int totalPrice = 0;
 
-        int totalItemsForSku = aggregated.get(sku);
-        int remainingForOffers = totalItemsForSku;
-        // sort offers array and find largest
+        int remainingForOffers = aggregated.get(sku);
         OfferSpecs[] offers = priceList.get(sku).getOfferSpecs();
         Arrays.sort(offers);
         for (OfferSpecs offer : offers) {
             if (remainingForOffers > offer.getOfferMultiple()) {
                 totalPrice += (remainingForOffers / offer.getOfferMultiple()) * offer.getOfferPrice();
-                remainingForOffers = 
+                remainingForOffers = remainingForOffers % offer.getOfferMultiple();
             }
         }
-        return -1;
+        totalPrice += remainingForOffers * priceList.get(sku).getPrice();
+        return totalPrice;
     }
 }
+
 
 
 
