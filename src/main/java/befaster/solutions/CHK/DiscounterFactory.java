4df43d452327126115aecfe20a525eec15d19005
sbx_user1051 <sbx_user1051@169.254.11.237>
2023-08-24 15:27:56 +0000

    Thu Aug 24 15:27:56 UTC 2023

---
+++ src/main/java/befaster/solutions/CHK/DiscounterFactory.java
@@ -0,0 +1,10 @@
+package befaster.solutions.CHK;
+
+import java.util.HashMap;
+import java.util.function.Supplier;
+
+public class DiscounterFactory {
+
+    private final HashMap<String, Supplier<Interface>>
+
+}
