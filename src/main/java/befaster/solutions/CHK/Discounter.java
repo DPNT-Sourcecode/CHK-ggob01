1d9ed579ffbdd81ba2fce015e2108b8053789f89
sbx_user1051 <sbx_user1051@169.254.11.237>
2023-08-24 15:12:56 +0000

    Thu Aug 24 15:12:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/Discounter.java
+++ src/main/java/befaster/solutions/CHK/Discounter.java
@@ -1,7 +1,13 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+
 public abstract class Discounter {
 
-    public Discounter(){}
-    public abstract int priceForSku();
+    private final HashMap<String, SkuPricing> priceList;
+    public Discounter(){
+        priceList = Helper.buildPriceList();
+    }
+    public abstract int priceForSku(String sku, HashMap<String, Integer> aggregated);
 }
+
--- src/main/java/befaster/solutions/CHK/SameProductDiscount.java
+++ src/main/java/befaster/solutions/CHK/SameProductDiscount.java
@@ -1,2 +1,25 @@
-package befaster.solutions.CHK;public class SameProductDiscount {
+package befaster.solutions.CHK;
+
+import java.util.HashMap;
+
+public class SameProductDiscount extends Discounter{
+
+    SameProductDiscount() {
+        super();
+    }
+
+    @Override
+    public int priceForSku(String sku, HashMap<String, Integer> aggregated) {
+        int totalPrice = 0;
+        pr
+
+        int singleChargeItems = aggregated.get(sku);
+        int offerMultiple = priceList.get(sku).getOfferMultiple();
+        if (offerMultiple > 0) {
+            totalPrice += (skuCount.getValue() / offerMultiple) * priceList.get(sku).getOfferPrice();
+            singleChargeItems = skuCount.getValue() % offerMultiple;
+        }
+        totalPrice += singleChargeItems * priceList.get(sku).getPrice();
+    }
 }
+
