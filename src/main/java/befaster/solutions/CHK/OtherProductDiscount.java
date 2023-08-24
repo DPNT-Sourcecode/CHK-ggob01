34afb7c20924842ba1cc302c348f965eab2e6182
sbx_user1051 <sbx_user1051@169.254.11.237>
2023-08-24 16:30:56 +0000

    Thu Aug 24 16:30:56 UTC 2023

--- src/main/java/befaster/solutions/CHK/OtherProductDiscount.java
+++ src/main/java/befaster/solutions/CHK/OtherProductDiscount.java
@@ -9,13 +9,13 @@ public class OtherProductDiscount extends Discounter{
 
         int singleChargeItems = aggregated.get(sku);
         int offerMultiple = priceList.get(sku).getOfferMultiple();
-        if (offerMultiple > 0) {
+        if (singleChargeItems > offerMultiple && offerMultiple > 0) {
             int offerMultiplier = aggregated.get(sku) / offerMultiple;
             totalPrice += aggregated.get(sku) * priceList.get(sku).getPrice();
             totalPrice -= (Math.min(offerMultiplier, aggregated.get("B"))) * priceList.get("B").getPrice();
-            singleChargeItems = aggregated.get(sku) % offerMultiple;
         }
         totalPrice += singleChargeItems * priceList.get(sku).getPrice();
         return totalPrice;
     }
 }
+
